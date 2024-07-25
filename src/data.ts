0aedcb72c6a67649c764397490ff1c9a637697b0
Artem Suchilin <temkafox@mail.ru>
2024-07-25 22:46:06 +0300

    fix questions

--- src/data.ts
+++ src/data.ts
@@ -25,4 +25,30 @@ export const data = [
       },
     ]
   },
+  {
+    questionNumber: 2,
+    questionText: 'Второй вопрос',
+    questionAnswers: [
+      {
+        id: 1,
+        text: '1',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: '2',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: '3',
+        isTrue: true,
+      },
+      {
+        id: 4,
+        text: '4',
+        isTrue: false,
+      },
+    ]
+  },
 ]
