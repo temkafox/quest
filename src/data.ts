3ac8f599e8bc61b4ad0ab36e3372845bccd44baa
Artem Suchilin <temkafox@mail.ru>
2024-07-26 13:57:24 +0300

    fix

--- src/data.ts
+++ src/data.ts
@@ -27,26 +27,364 @@ export const data = [
   },
   {
     questionNumber: 2,
-    questionText: 'Второй вопрос',
+    questionText: 'Кто его любимая знаменитость?',
     questionAnswers: [
       {
         id: 1,
-        text: '1',
+        text: 'Меган Фокс, потому что она подожгла свой язык в фильме «Тело Дженифер»',
         isTrue: false,
       },
       {
         id: 2,
-        text: '2',
+        text: 'Мэттью Макконахи, потому что он классно плакал в фильме «Интерстеллар»',
+        isTrue: true,
+      },
+      {
+        id: 3,
+        text: 'Скарлет Йоханссон, потому что она виртуозно стала искусственным интеллектом в фильме «Люси» ',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'Райан Гослинг, потому что он делал крутые трюки в фильме «Драйв»',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 3,
+    questionText: 'Назови самое необычное место, в котором вы занимались сексом.',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'В гостях',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'В самолете',
         isTrue: false,
       },
       {
         id: 3,
-        text: '3',
+        text: 'В машине',
         isTrue: true,
       },
       {
         id: 4,
-        text: '4',
+        text: 'До свадьбы нельзя :)',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 4,
+    questionText: 'Что он считает твоей самой раздражающей привычкой?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Спорить с Толей»',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'Не успевать вовремя собраться',
+        isTrue: true,
+      },
+      {
+        id: 3,
+        text: 'Не спрашивать мнение Толи',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'Засыпать в ванной',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 5,
+    questionText: 'А какая скверная привычка есть у него?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Ругаться, когда не выходите вовремя',
+        isTrue: true,
+      },
+      {
+        id: 2,
+        text: 'Считать, что всегда прав во всем',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'Становиться злым, когда голоден',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'Не думать о тебе, когда созрел план досуга',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 6,
+    questionText: 'В каком году вы начали встречаться?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: '2001',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: '2012',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: '2020',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: '2021',
+        isTrue: true,
+      },
+    ]
+  },
+  {
+    questionNumber: 7,
+    questionText: 'На каком по счету свидании вы занялись любовью?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'До 1 свидания',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'на 1',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'на 2',
+        isTrue: true,
+      },
+      {
+        id: 4,
+        text: 'До свадьбы нельзя :)',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 8,
+    questionText: 'Как ему больше нравится засыпать: в обнимку с вами или отвернувшись?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'На 20% в обнимку и на 80% отвернувшись',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: ' На 80% в обнимку и на 20% отвернувшись',
+        isTrue: true,
+      },
+      {
+        id: 3,
+        text: 'На 65% в обнимку и на 35% отвернувшись',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'На 35% в обнимку и на 65% отвернувшись',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 9,
+    questionText: ' С какого раза он сдал на права?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'С 1 раза',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'Со 2 раза',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'С 3 раза',
+        isTrue: true,
+      },
+      {
+        id: 4,
+        text: 'Не сдал',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 10,
+    questionText: 'Какого цвета у него военный билет?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Красный',
+        isTrue: true,
+      },
+      {
+        id: 2,
+        text: 'Синий',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'Зелёный',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'Коричневый',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 11,
+    questionText: ' Какое его любимое блюдо, которое готовишь ты?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Овощной салатик',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'Куриный супчик',
+        isTrue: true,
+      },
+      {
+        id: 3,
+        text: 'Стейк',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'Макарошки с сыром',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 12,
+    questionText: 'Что бы он назвал твоим самым позорным/кринжовым поступком?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Когда сильно смеялась и вылетела сопля',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'Писала в бассейне',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'Сказала «какая булочка» в присутствии полной дамы',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'Таких нет',
+        isTrue: true,
+      },
+    ]
+  },
+  {
+    questionNumber: 13,
+    questionText: 'Какой фильм стал самым запоминающимся для Толи, на который вы ходили в кино?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Бременские музыканты',
+        isTrue: true,
+      },
+      {
+        id: 2,
+        text: 'Главный герой',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'Переводчик',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'Вызов',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 14,
+    questionText: 'Какое у него любимое время года?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Лето и весна',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'Весна и осень',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'Зима и лето',
+        isTrue: true,
+      },
+      {
+        id: 4,
+        text: 'Любит все времена года',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 15,
+    questionText: 'Что именно он сказал, когда делал тебе предложение?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Хочу провести с тобой всю жизнь, потому что ты именно то, что мне нужно',
+        isTrue: true,
+      },
+      {
+        id: 2,
+        text: 'Выходи за меня',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'Я искал это кольцо так долго, что ты просто обязана выйти за меня замуж',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: '"...Что-то невнятное...", ты станешь моей женой?',
         isTrue: false,
       },
     ]
